e0e0f9e6cbe364c80c6914fcf9df191196e3c671
Muhammadali1406 <rmuhammadali316@gmail.com>
2025-09-20 15:38:47 +0500

    changed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,9 +10,9 @@ function Todo({todos}) {
    }
    return <ul>
     {todo.map(el=>{
-        return <li key={el.id}>
+        return <li className='w-full p-3 text-2xl font-bold text-blue-900 flex justify-between' key={el.id}>
             <h3>{el.todo}</h3>
-            <button onClick={()=>deleteTodo(el.id)} className='p-3 rounded-[5px] bg-red-700 text-white' type='button'>delete</button>
+            <button onClick={()=>deleteTodo(el.id)} className=' w-[20%] p-3 rounded-[5px] bg-red-700 text-white' type='button'>delete</button>
         </li>
     })}
    </ul>
