56d05627210210f42b70852500bf20e89965ae40
Muhammadali1406 <rmuhammadali316@gmail.com>
2025-09-20 15:43:20 +0500

    changes all changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,9 +8,9 @@ function Todo({todos}) {
     settodo(newtodos)
     
    }
-   return <ul>
+   return <ul className='flex flex-col gap-4 justify-center'>
     {todo.map(el=>{
-        return <li className='w-full max-w-[800px] p-3 text-2xl font-bold text-blue-900 flex justify-between' key={el.id}>
+        return <li className='w-full bg-amber-50 rounded-[12px] max-w-[800px] p-3 text-2xl font-bold text-blue-900 flex justify-between' key={el.id}>
             <h3>{el.todo}</h3>
             <button onClick={()=>deleteTodo(el.id)} className=' w-[20%] p-3 rounded-[5px] bg-red-700 text-white' type='button'>delete</button>
         </li>
